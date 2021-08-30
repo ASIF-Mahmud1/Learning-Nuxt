4be7c32f49229a15ca9769357d4bf93b429bd690
Asif Mahmud <asif01050105@gmail.com>
2021-08-31 00:32:51 +0600

    #4 [in progress] todo app

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+   
+  <todo-list v-bind:todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  },
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project Nice',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project Bad',
+        done: true,
+      }, {
+        title: 'Todo Cool',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project Demanding',
+        done: false,
+      }],
+    };
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div> 
+      <div>
+        {{ todo.title }}       {{ todo.project }}
+      </div>
+     
+      <div v-show="todo.done">Completed</div>
+      <div v-show="!todo.done">Complete</div>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+export default {
+  props: ["todo"],
+};
+</script>
+<style>
+</style>
